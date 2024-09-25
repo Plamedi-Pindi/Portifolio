64116321fa5249b6746e839f40d55fc9d11d9fab
Plamedi-Pindi <plamedipindi@gmail.com>
2024-09-25 17:36:52 +0100

    deploy pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path='/Portifolio' element={<Layout />}>
            <Route index element={<Home />} /> 
         </Route>
       </Routes>
